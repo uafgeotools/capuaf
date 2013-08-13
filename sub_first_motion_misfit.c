616ff2ad848e232df36b088b3c0bf13f0eb4cad3
Celso Alvizuri <celso.alvizuri@gmail.com>
2013-08-13 01:33:02 +0000

    code to compute misfit from first motion polarity
    git-svn-id: svn+ssh://badger.giseis.alaska.edu/home/admin/repositories/GEOTOOLS@1137 3ab8ff1a-8c7a-11e0-a7ea-c5af1200b721

---
+++ sub_first_motion_misfit.c
@@ -0,0 +1,70 @@
+/* =============================================================================
+ *
+ *       Filename:  sub_misfit_fm.c
+ *
+ *        Created:  08/08/2013 03:24:46 PM
+ *         Author:  celso r. a.
+ *
+ *    Description:  this subroutine computes predicted P-first motion polarity
+ *                  at each station
+ *          Input:  mtensor, nfm, fm (struct), fid,
+ *                  gamma, delta, strike, dip, rake
+ *         Output:  (to file)
+ *                  gamma, delta, strike, dip, rake, P1 P2 P3 ... PN
+ *                  (N station polarities)
+ *
+ * =============================================================================
+ */
+#include<stdio.h>
+#include"cap.h"
+
+void misfit_first_motion(
+            float mtensor[3][3],
+              int nsta,
+               FM *data,
+             FILE *fid,
+            float gamma,    // clvd. [-30,30] degrees
+            float delta,    // isotropic. [0,180] degrees (note alternative delta [-90,90])
+            float mw,       // not used but included here in case of future use
+            float kappa,    // strike
+            float theta,    // dip
+            float sigma     // slip
+        )
+{
+    float pol_theory;           // predicted polarity
+    int ipol_theory, ipol_obs;  // integer polarities
+    int misfit_fm=0;
+    int debug_fm=0;             // 1: output predicted polarities. 0: don't output
+    int i;
+
+    // compute first motion misfit = sum(abs(obs_i-predicted_i))
+    // output grid search parameters, polarity (in debug mode), and misfit
+    // data written to fid (opened in cap.c)
+    fprintf(fid,"%5.1f %5.1f %5.1f %5.1f %5.1f  ", gamma, delta, kappa, theta, sigma);
+
+    if(debug_fm)
+    {
+        for(i=0; i<nsta; i++)
+        {
+            ipol_obs = (data+i)->type;
+            pol_theory = radpmt(mtensor, (data+i)->alpha, (data+i)->az, 1);
+            if(pol_theory>0) ipol_theory = 1;
+            else ipol_theory = -1;
+            misfit_fm += abs(ipol_obs-ipol_theory);
+            fprintf(fid,"%d ",ipol_theory);
+        }
+    }
+    else
+    {
+        for(i=0; i<nsta; i++)
+        {
+            ipol_obs = (data+i)->type;
+            pol_theory = radpmt(mtensor, (data+i)->alpha, (data+i)->az, 1);
+            if(pol_theory>0) ipol_theory = 1;
+            else ipol_theory = -1;
+            misfit_fm += abs(ipol_obs-ipol_theory);
+        }
+    }
+    misfit_fm = misfit_fm/nsta;
+    fprintf(fid,"\t%d\n",misfit_fm);
+}
