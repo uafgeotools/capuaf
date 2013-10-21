7d8d3519fb5b881717d3d9999e6c27c39c7134ed
Carl Tape <carltape@gmail.com>
2013-10-21 22:20:39 +0000

    comments describing Mw to M0 conversion
    git-svn-id: svn+ssh://badger.giseis.alaska.edu/home/admin/repositories/GEOTOOLS@1286 3ab8ff1a-8c7a-11e0-a7ea-c5af1200b721

--- cap.c
+++ cap.c
@@ -587,6 +587,8 @@ int main (int argc, char **argv) {
 	  (int) rint(rad[0]), (int) rint(rad[1]), (int) rint(rad[2]),
 	  mt[1].par, sqrt(mt[1].sigma*x2),mt[2].par, sqrt(mt[2].sigma*x2));
   fprintf(f_out,"# Variance reduction %4.1f\n",100*(1.-sol.err/data2));
+  // convert Mw to M0 using GCMT convention (also in Aki and Richards, 2002)
+  // this is very close to Kanamori1977 (16.1 vs 16.1010)
   amp=pow(10.,1.5*mt[0].par+16.1-20);
 
   if(search) {
