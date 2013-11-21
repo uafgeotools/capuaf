52c41b59966111f074221ab3b23ceb656757c16f
Carl Tape <carltape@gmail.com>
2013-11-21 01:09:39 +0000

    added check directory for uturuncu example; restored $green = "$caprun/models" as default in cap.pl
    git-svn-id: svn+ssh://badger.giseis.alaska.edu/home/admin/repositories/GEOTOOLS@1403 3ab8ff1a-8c7a-11e0-a7ea-c5af1200b721

--- cap.pl
+++ cap.pl
@@ -21,8 +21,8 @@ $cmd = "cap";
 
 # green's function location
 #$green = "$home/data/models/Glib";        # original
-$green = "/store/wf/FK_synthetics";       # standard models at UAF
-#$green = "$caprun/models";                # user testing
+#$green = "/store/wf/FK_synthetics";       # standard models at UAF
+$green = "$caprun/models";                # user testing
 
 $repeat = 0;
 $bootstrap = 0;
