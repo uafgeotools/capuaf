a297b1e44acc31bd0236a459ac4eeb3fdd6b6402
cralvizuri <cralvizuri@alaska.edu>
2016-01-15 20:41:24 -0900

    functions to get (u,v) for uniformMT (ongoing)

---
+++ uv2lune.c
@@ -0,0 +1,216 @@
+/* 
+   Functions to compute (gamma, delta) from (u,v)
+   These are called from function initSearchMT
+
+   For (u,v) parameterization see 
+   "A uniform parameterization of moment tensors" (Tape & Tape, 2015)
+
+   20160108 celso alvizuri - cralvizuri@alaska.edu 
+
+*/
+
+#include "uv2lune.h"
+
+/* compute beta(u) in vector form */
+void u2beta_vec(float *pxa, float *pya, int na, float *pArray_u, float *pArray_beta, int nsol)
+{
+    //interp1(xa, npts, ya, xi, nu2beta_out, yi);             // previous
+    fprintf(stderr,"create vector u2beta...");
+    interp_lin(pxa, pya, na, pArray_u, pArray_beta, nsol);   // best
+    //    fprintf(stderr,"done\n");
+}
+
+float beta2delta(float beta)
+{
+    float delta;
+    delta = beta - (PI / 2.0);     //delta = beta - 90.0;
+//    fprintf(stdout,"### CHECK DELTA. %f \n", delta * r2d);
+    return delta;
+}
+
+/* compute delta=delta(beta) */
+void beta2delta_vec(float *pArray_beta, float *pArray_delta, int nsol)
+{
+    int i;
+    fprintf(stderr,"create vector beta2delta... ");
+    for(i=0; i<nsol; i++) {
+        // convert beta to delta
+        pArray_delta[i] = beta2delta(pArray_beta[i]);
+    }
+    fprintf(stderr,"done. NPTS = %d\n", i);
+}
+
+/* compute gamma(v) */
+float v2gamma(float v)
+{
+    float gamma;
+    float k3 = 1./3;
+
+    // sanity check range for v
+    if((v < -k3) || (v > k3)) {
+        fprintf(stderr,"WARNING. v= %5.2e is outside range\n");
+        return -1;
+    }
+    gamma = k3 * asinf(3. * v);
+//    fprintf(stdout,"### CHECK GAMMA. %f \n", gamma * r2d);
+    return gamma;
+}
+
+/* compute vector of gamma values */
+void v2gamma_vec(float *pArray_v, int nsol, float *pArray_gamma)
+{
+    int i;
+    fprintf(stderr,"create vector v2gamma ... ");
+    for(i = 0; i < nsol; i++) {
+        pArray_gamma[i] = v2gamma(pArray_v[i]);
+    }
+    fprintf(stderr,"done.\tNPTS = %d\n", i);
+    //fprintf(stderr,"routine: %s line %d \n", __FILE__, __LINE__);
+}
+
+/* convert h to dip */
+float h2dip(float h)
+{
+    return acosf(h);
+}
+
+/* get vector of dip values */
+void h2dip_vec(float *pArray_h, int nsol, float *pArray_dip)
+{
+    int i;
+    fprintf(stderr,"create vector h2dip... ");
+    for(i=0; i<nsol; i++) {
+        pArray_dip[i] = h2dip(pArray_h[i]);
+    }
+    fprintf(stderr,"done.\tNPTS = %d\n", i);
+}
+
+/* 
+    Create vector of random points in a range.
+    There may be more robust implementations.
+    Consider using other than drand48. 
+        it needs to be converted to float
+        it may not be available in other systems like Windows (may not matter...)
+        rand or random may be more actively developed.
+ */
+void randvec(float xmin, float xmax, int nsol, float *pArray)
+{
+    int i;
+    float width;
+    /* srand(12345); */     /* use with random */ 
+
+    width = (xmax - xmin);
+    fprintf(stderr,"create RAND vector. xmin= %10.5f xmax= %10.5f NPTS = %d ... ", xmin, xmax, nsol);
+    for(i = 0; i < nsol; i++) {
+        pArray[i] = ((float)drand48() * width) + xmin;
+    }
+    fprintf(stderr,"\tdone. NSOL = %d\n", i);
+}
+
+/* create vector of grid points in a range */
+/* 
+    NOTE uniformMT.m applies offset to full range AND subrange
+         shouldn't it apply only to full range?
+*/
+void gridvec(float xmin, float xmax, int npoints, float *pArray)
+{
+    int i;
+    float dx, xoffset;
+
+    /* get dx based on original user input */
+    dx = (xmax - xmin) / (float) (npoints - 1); // NOTE npts-1
+
+    /* Offset from boundaries.
+     * Factor 2.0 looks arbitrary. it could be made smaller.
+     * Also it's not clear why dx should scale with the number of points. 
+     * ie why not just make this a tiny constant. */
+    xoffset = dx/2.0;
+
+    fprintf(stderr, "create GRID vector. xmin= %10.5f xmax= %10.5f dx= %12.5e ... (old)\n", xmin, xmax, dx);
+    /* apply offset at boundaries and recalculate dx based on new boundaries */
+    xmin = xmin + xoffset;
+    xmax = xmax - xoffset;
+    dx = (xmax - xmin) / (float) (npoints - 1);
+
+    fprintf(stderr, "create GRID vector. xmin= %10.5f xmax= %10.5f dx= %12.5e ... (new)", xmin, xmax, dx);
+    for(i = 0; i < npoints; i++) {
+        pArray[i] = xmin + dx * (float) i;
+//        fprintf(stdout,"### CHECK GRIDVEC. %f \n", pArray[i] );
+    }
+    if((pArray[npoints-1] - xmax) > 0.001) {
+        fprintf(stderr,"WARNING. end point does not match expected end point!\n");
+        fprintf(stderr,"xmax(actual) = %f. xmax(expected) = %f\n", pArray[npoints-1], xmax);
+    }
+    fprintf(stderr,"\tdone. NPTS = %d\n", i);
+}
+
+/*
+    This is brute force and slow. The gnu method is faster
+    [timings]
+    But the request is to not add more library dependencies (gnu) to CAP for now.
+*/
+int find_nearest_index(float value, float * pData, int len)
+{
+    float dist, newDist;
+    int inearest;
+    int i;
+
+    inearest = -1;  // if negative then nearest index not found
+    dist = DBL_MAX;
+
+    for(i = 0; i < len; i++) {
+        newDist = value - pData[i]; // negative dist means 'value' is to the left of pData[i]
+        // check that distances are not negative (check when this becomes an issue)
+        if ((newDist > 0) && (newDist < dist)) {
+            dist = newDist;
+            inearest = i;
+        }
+    }
+
+    return inearest;
+}
+
+void interp_lin(float *x,     // x array  (input)
+                float *y,     // y array  (input)
+                int x_size,   // size     (input)
+                float *xx,    // xx array (input)
+                float *yy,    // yy array (output)
+                int xx_size)  // size     (input)
+{
+    float dx, dy, *slope, *intercept;
+    int i, index_nearest;
+
+    slope = (float *) calloc(x_size, sizeof(float));
+    intercept = (float *) calloc(x_size, sizeof(float));
+
+    // Precompute slopes and intercepts for all elements in x and y
+    // the last elements in x, y are made equal to the previous element
+    for(i = 0; i < x_size; i++) {
+        if(i < x_size-1) {
+            dx = x[i + 1] - x[i];
+            dy = y[i + 1] - y[i];
+            slope[i] = dy / dx;
+            intercept[i] = y[i] - x[i] * slope[i];
+        } else {
+            slope[i] = slope[i-1];
+            intercept[i] = intercept[i-1];
+        }
+    }
+
+    fprintf(stderr,"\tcalculating values beta(u) ... ");
+    for (i = 0; i < xx_size; i++) {
+        index_nearest = find_nearest_index(xx[i], x, x_size);
+
+        // compute all interpolations yy for xx
+        if (index_nearest != -1) {
+            yy[i] = slope[index_nearest] * xx[i] + intercept[index_nearest];
+//            fprintf(stderr,"### CHECK. inearest= %d xi= %f yi= %f \n", index_nearest, x[i], y[i]); 
+        }
+        else
+            yy[i] = DBL_MAX;
+    }
+    fprintf(stderr,"done. NPTS = %d\n", i);
+    free(slope);
+    free(intercept);
+}
+
---
+++ uv2lune.h
@@ -0,0 +1,18 @@
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>  // drand48
+#include<float.h> 
+
+#include "cap.h"
+
+void u2beta_vec(float *pxa, float *pya, int na, float *pArray_u, float *pArray_beta, int nsol);
+float beta2delta(float beta);
+void beta2delta_vec(float *pArray_beta, float *pArray_delta, int nsol);
+float v2gamma(float v);
+void v2gamma_vec(float *pArray_v, int nsol, float *pArray_gamma);
+float h2dip(float h);
+void h2dip_vec(float *pArray_h, int nsol, float *pArray_dip);
+void randvec(float xmin, float xmax, int nsol, float *pArray);
+void gridvec(float xmin, float xmax, int npoints, float *pArray);
+int find_nearest_index(float value, float * pData, int len);
+void interp_lin(float *x, float *y, int x_size, float *xx, float *yy, int xx_size);
