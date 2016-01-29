a140444662004a8406b8b37f2433b1ce2d5b8580
cralvizuri <cralvizuri@alaska.edu>
2016-01-28 17:24:52 -0900

    header for tt2cmt

---
+++ sub_tt2cmt.h
@@ -0,0 +1,16 @@
+// header file for sub_tt2cmt.c
+
+
+#include<stdio.h>
+#include<math.h>
+
+#define pi 3.1415926535897932385
+#define kR3 sqrt(3.0)
+#define k2R6 2.0 * sqrt(6.0)
+#define k2R3 2.0 * sqrt(3.0)
+#define k4R6 4.0 * sqrt(6.0)
+#define k8R6 8.0 * sqrt(6.0)
+#define deg2rad pi / 180.0
+
+#define m0scale 1.0
+//#define m0scale 0.707106781186547       // if we really want norm(mtensor) = |1|
