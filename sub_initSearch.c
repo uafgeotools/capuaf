0b43d363f5a85e6272dd134c58e84f047c86e5a1
cralvizuri <cralvizuri@alaska.edu>
2016-02-15 15:37:43 -0900

    add comments + cleanup

--- sub_initSearch.c
+++ sub_initSearch.c
@@ -1,19 +1,31 @@
-#include "cap.h"
 /*
-  Function to search uniform moment tensors
-       initSearchMT
- 
-  Function initSearchMT calls functions
-       get_Rand_MT
-       get_Grid_MT
-       search_MT
- 
-  See uniformMT.m for examples to generate uniform moment tensors in matlab.
-  See also "A uniform parameterization of moment tensors" (Tape & Tape, 2015)
- 
-  20160112 celso alvizuri - cralvizuri@alaska.edu 
+
+    The following routines perform an inversion for the optimal seismic moment
+    tensor, and output data for estimating the moment tensor uncertainty.
+    The moment tensors are uniformly distributed when specifying the full range
+    for (gamma, delta, strike, dip, rake).
+    File sub_inversion.c is composed of functions initSearchMT, getRandMT, 
+    getGridMT, searchMT described below. sub_inversion.c replaces the old 
+    "error" function in cap.
+
+    initSearchMT -- Select inversion type
+    getRandMT    -- Create a set of moment tensors randomly
+    getGridMT    -- Create a set of moment tensors on a grid
+    searchMT     -- Main function. 
+                    Search the optimal moment tensor.
+                    Output data for uncertainty estimation.
+                    Compile with openMP to run in parallel.
+
+    Main references
+    Matlab routine uniformMT.m
+    Tape, W., and C. Tape (2015), A uniform parametrization of moment tensors,
+    Geophys. J. Int., 202, 2074-2081.
+
+    20160112 celso alvizuri - cralvizuri@alaska.edu 
  
  */
+
+#include "cap.h"
 
 SOLN initSearchMT( int npar, // 3=mw; 2=iso; 1=clvd; 0=strike/dip/rake
         int nda,
@@ -149,7 +161,9 @@ void getRandMT(SEARCHPAR * searchPar, ARRAYMT * arrayMT)
         arrayMT[isol].kappa = vec_kappa[isol];
         arrayMT[isol].theta = vec_theta[isol];
         arrayMT[isol].sigma = vec_sigma[isol];
-//        fprintf(stdout,"index = %20d %f %f %f %f %f \n", isol, arrayMT[isol].gamma *r2d, arrayMT[isol].delta *r2d,  arrayMT[isol].kappa *r2d,  arrayMT[isol].theta *r2d,  arrayMT[isol].sigma *r2d);
+
+        // output values for debugging
+        // fprintf(stdout,"index = %20d %f %f %f %f %f \n", isol, arrayMT[isol].gamma *r2d, arrayMT[isol].delta *r2d,  arrayMT[isol].kappa *r2d,  arrayMT[isol].theta *r2d,  arrayMT[isol].sigma *r2d);
     }
     fprintf(stderr,"done. nsol = %d \n", isol);
     if (isol == 1) {
@@ -258,11 +272,14 @@ void getGridMT(SEARCHPAR * searchPar, ARRAYMT * arrayMT)
                         arrayMT[isol].kappa = vec_kappa[ik];
                         arrayMT[isol].theta = vec_theta[ih];
                         arrayMT[isol].sigma = vec_sigma[is];
-//                        fprintf(stdout,"index= %20d %11.6f %11.6f %11.6f %11.6f %11.6f\n",
-//                                isol, arrayMT[isol].gamma*r2d, arrayMT[isol].delta*r2d, arrayMT[isol].kappa*r2d, arrayMT[isol].theta*r2d, arrayMT[isol].sigma*r2d);
-                        //fprintf(stdout,"index= %20d %6d %6d %6d %6d %6d\n", isol, ig, id, ik, ih, is);
-                        //fprintf(stdout,"index= %20d %11.6f %11.6f %11.6f %11.6f %11.6f\n",
-                        //        isol, vec_gamma[ig]*r2d, vec_delta[id]*r2d, vec_kappa[ik]*r2d, vec_theta[ih]*r2d, vec_sigma[is]*r2d);
+        // output values for debugging
+        // fprintf(stdout,"index= %20d %11.6f %11.6f %11.6f %11.6f %11.6f\n",
+        //                isol, arrayMT[isol].gamma*r2d, arrayMT[isol].delta*r2d, arrayMT[isol].kappa*r2d, arrayMT[isol].theta*r2d, arrayMT[isol].sigma*r2d);
+        // output values for debugging
+        // fprintf(stdout,"index= %20d %6d %6d %6d %6d %6d\n", isol, ig, id, ik, ih, is);
+        // output values for debugging
+        // fprintf(stdout,"index= %20d %11.6f %11.6f %11.6f %11.6f %11.6f\n",
+        //        isol, vec_gamma[ig]*r2d, vec_delta[id]*r2d, vec_kappa[ik]*r2d, vec_theta[ih]*r2d, vec_sigma[is]*r2d);
                         isol++;
                     }
                 }
@@ -348,11 +365,9 @@ SOLN searchMT( int npar, // 3=mw; 2=iso; 1=clvd; 0=strike/dip/rake
     fidbb=fopen(outFileBB,"wb");
 #endif
 
-    // count number of solutions computed
-    // (this is ongoing, this variable may be deleted)
-    // # 20151216 celso alvizuri - cralvizuri@alaska.edu
-    int count=0;
-
+    // START DELETE SECTION
+    // KEEP?
+    // Used in previous cap to generate misfit on the lune (Uturuncu FMT paper)
     /* vars to track smallest misfit at each (gamma,delta) on the lune */
     FILE *fidmol;
     //    LUNE_MISFIT * bestmisfit;
@@ -368,22 +383,12 @@ SOLN searchMT( int npar, // 3=mw; 2=iso; 1=clvd; 0=strike/dip/rake
     {
         fidfmp=fopen("out.misfit.fmp_","w");
     }
-
-    /* TODO these will come from user input */
-    //temp[0] = 4.5; // ALASKA
-    //temp[0] = 2.8; // UTURUNCU
-    //temp[0] = 5.2; // ILLINOIS
-    // temp[0] is set to starting magnitude (when no magnitude search).
-    // another option is define point mag mw0 if point mag search
-    //temp[0] = searchPar->mw1;
+    // END DELETE SECTION
 
     // prepare magnitude array
     // NOTE magnitude points include boundaries (unlike gridvec)
     float * vec_mag = calloc(searchPar->nmw, sizeof(float));
     magvec(searchPar->mw1, searchPar->mw2, searchPar->dmw, vec_mag);
-
-    //amp = pow(10.,1.5 * +16.1-20);
-    //amp = pow(10.,1.5*temp[0]+16.1-20);
 
     grd_err = grid.err;
     best_sol.err = FLT_MAX;
@@ -394,27 +399,18 @@ SOLN searchMT( int npar, // 3=mw; 2=iso; 1=clvd; 0=strike/dip/rake
     nmag = searchPar->nmw; 
     for(imag = 0; imag < nmag; imag++) {
         fprintf(stderr,"Loop Mw[%d/%d] = %6.3f\n", imag+1, nmag, vec_mag[imag]);
-        //        temp[0] = vec_mag[imag];
         amp = pow(10., 1.5 * vec_mag[imag] + 16.1 - 20);
 
         // LOOP THROUGH ALL SOLUTIONS
         // MAIN LOOP 
+        // WARNING Anything inside the following loop will likely run millions of times.
+        // WARNING Consider carefully before adding any code inside this loop.
+
         nreject = 0;
 #ifdef OMP
 #pragma omp parallel for private(sol, mtensor)
 #endif
         for(isol = 0; isol < searchPar->nsol; isol++) {
-            /*
-               TODO output coords (u,v) <--> (v,w)
-               (v,w) should be straightforward from (u,v)
-               w = 3pi/8 - u
-               */
-
-            //           temp[2] = arrayMT[isol].gamma * r2d;
-            //           temp[1] = arrayMT[isol].delta * r2d;
-            //           sol.meca.stk = arrayMT[isol].kappa * r2d;
-            //           sol.meca.dip = arrayMT[isol].theta * r2d;
-            //           sol.meca.rak = arrayMT[isol].sigma * r2d;
 
             //tt2cmt(temp[2], temp[1], 1.0, sol.meca.stk, sol.meca.dip, sol.meca.rak, mtensor);
             tt2cmt(arrayMT[isol].gamma * r2d, arrayMT[isol].delta * r2d, 1.0, arrayMT[isol].kappa * r2d, arrayMT[isol].theta * r2d, arrayMT[isol].sigma * r2d, mtensor);
@@ -428,18 +424,15 @@ SOLN searchMT( int npar, // 3=mw; 2=iso; 1=clvd; 0=strike/dip/rake
             }
 
             // compute misfit from first motion. data will be output to out.misfit.fmp_
-            // misfit_fmp = misfit_first_motion(mtensor, nfm, fm, fidfmp, temp[2], temp[1], temp[0], sol.meca.stk, sol.meca.dip, sol.meca.rak);
             misfit_fmp = misfit_first_motion(mtensor, nfm, fm, fidfmp, arrayMT[isol].gamma * r2d, arrayMT[isol].delta * r2d, vec_mag[imag], sol.meca.stk, sol.meca.dip, sol.meca.rak);
 
-            //--------------KEY COMMAND---call misfit function------
+            // KEY COMMAND. compute waveform misfit
             sol=calerr(nda,obs0,max_shft,tie,norm,mtensor,amp,sol);
 
+            // The misfit for this solution
             // Ncomp = number of components.
-            sol.err=sol.err/Ncomp;
-            //*grd_err++ = sol.err; /*error for this solution*/
-            //
+            sol.err = sol.err/Ncomp;
 
-            //        fprintf(stdout,"index= %10d\t%11.6f %11.6f %11.6f %11.6f %11.6f\n", isol, temp[2], temp[1], sol.meca.stk, sol.meca.dip, sol.meca.rak);
             if (best_sol.err > sol.err) {
                 isol_best = isol;
 #ifdef OMP
@@ -462,12 +455,10 @@ SOLN searchMT( int npar, // 3=mw; 2=iso; 1=clvd; 0=strike/dip/rake
                         sol.meca.mag, sol.meca.gamma, sol.meca.delta, sol.meca.stk, sol.meca.dip, sol.meca.rak,
                         sol.err, VR);
 
-                /* output variables (only use for debug) */
-                /*
-                   fprintf(stderr,"best sol isol=%9d (%3d%) sol.err= %13.6e mag=%5.2f %11.6f %11.6f %11.6f %11.6f %11.6f\n", 
-                   arrayMT[isol].gamma * r2d, arrayMT[isol].delta * r2d, 
-                   arrayMT[isol].kappa * r2d, arrayMT[isol].theta * r2d, arrayMT[isol].sigma * r2d);
-                   */
+                // output values for debugging
+                //fprintf(stderr,"best sol isol=%9d (%3d%) sol.err= %13.6e mag=%5.2f %11.6f %11.6f %11.6f %11.6f %11.6f\n", 
+                //arrayMT[isol].gamma * r2d, arrayMT[isol].delta * r2d, 
+                //arrayMT[isol].kappa * r2d, arrayMT[isol].theta * r2d, arrayMT[isol].sigma * r2d);
             }
 
             //  output binary data
