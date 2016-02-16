db81dfab18a10c26e474623ee179e79bde6539c2
cralvizuri <cralvizuri@alaska.edu>
2016-02-15 15:57:33 -0900

    cleanup commented code

--- cap.c
+++ cap.c
@@ -104,7 +104,7 @@ int skip_zero_weights=1;    // for original CAP set skip_zero_weights=1
 // Function gridvec does not implement the discretization of the previous version of 
 // cap.c which uses rules to account for special grid points.
 // Function gridvec also avoids endpoints in all parameters.
-// See NOTE flag LUNE_GRID_INSTEAD_OF_UV in function sub_initSearch.c
+// See NOTE flag LUNE_GRID_INSTEAD_OF_UV in function sub_inversion.c
 int LUNE_GRID_INSTEAD_OF_UV = 0;    // default = 0 (ie do not run old grid mode)
 
 int main (int argc, char **argv) {
@@ -142,7 +142,7 @@ int main (int argc, char **argv) {
   // option 2 seed using current time
   //srand(time(NULL));
 
-  /* START block of variables for uniform MT */
+  // variables for uniform MT
   SEARCHPAR *searchPar = calloc(1, sizeof(SEARCHPAR));
 
 //    float v1, v2, w1, w2, k1, k2, h1, h2, s1, s2;
@@ -159,7 +159,6 @@ int main (int argc, char **argv) {
 
 //    int nsol;
   /* END block of variables for uniform MT */
-
   // variables to verify that Mw_best is not near search limits
   FILE * fid_warn;        // output file for warnings
 
@@ -253,24 +252,6 @@ int main (int argc, char **argv) {
   /** and tie of time shifts between SH and P-SV **/
 
   /** begin -- get range of search parameters **/
-  //--------------------------------------------------------
-  // mt[0] = Magnitude search
-  // mt[1] = Isotropic 
-  // mt[2] = CLVD
-  //
-  // grid.step[0] = step size for strike
-  // grid.step[1] = step size for dip
-  // grid.step[2] = step size for rake
-  //--------------------------------------------------------
-  // See error function:  (mw_ran = 0.5; mt[0].max = mt[0].par+mw_ran; mt[0].min = mt[0].par-mw_ran; mw_ran = 0.5;)
-  // mt[0].par = Best magnitude value so far (Center of the magnitude search range (From -M flag); 
-  // mt[1].par = Best ISO value so far (starts from the minimum of search range mt[1].min)
-  // mt[2].par = Best CLVD value so far (starts from the minimum of search range mt[2].min)
-  //
-  // mt[0].dd  = magnitude increment (From -I flag)
-  // mt[1].dd  = ISO increment (From -I flag)
-  // mt[2].dd  = CLVD increment (From -I flag) (ALWAYS equal to ISO increment 'mt[1].dd') 
-
   fprintf(stderr, "\nInput parameter ranges \n");
   // NOTE magnitude parameters include dMw and number of points
   scanf("%f%f%d%f", &searchPar->mw1, &searchPar->mw2, &searchPar->nmw, &searchPar->dmw);
@@ -296,77 +277,6 @@ int main (int argc, char **argv) {
   fprintf(stderr, "s1= %11.6f s2= %11.6f ns= %10d ds= %10d\n", searchPar->s1, searchPar->s2, searchPar->ns, searchPar->ds);
   fprintf(stderr, "\nNumber of solutions to prepare = %10d\n", searchPar->nsol);
 
-//  return(0);
-
-  /*
-  scanf("%f%f%f", &(mt[0].par), &(mt[0].dd), &(mt[1].dd));
-  // See cap.pl for : ($mg $dm $dlune)
-  mt[0].min =  1.;  mt[0].max = 10.; // Maximum and Minimum of magnitude search range
-
-  // mt[0] is center magnitude. save copy for checking final result
-  mw_center = mt[0].par;
- 
-  if(search != 0) {
-      // use ranges for lune parameters if "search = 1 or 2" (grid or random search)
-      scanf("%f%f", &(mt[1].min), &(mt[1].max));  // -90 to 90 (Isotropic range)
-      scanf("%f%f", &(mt[2].min), &(mt[2].max));  // -30 to 30 (CLVD range)
-      if (search==1){ 
-          // Grid search
-          if ((mt[1].min==0.) && (mt[1].max==0.) && (mt[2].min==0.) && (mt[2].max==0.) && (mt[1].dd != 0.)){
-              // If Lune increment 'mt[1].dd == 0' then only double couple (example: -I10/0.1)
-              // If Lune increment 'mt[1].dd != 0'  then FMT  (example: -I10/0.1/10)
-              // Note: specifying magnitude increment is optional; 
-              fprintf(stderr,"Warning: Range not specified (-J flag); Doing full grid search\n");
-              mt[1].min=-90.; mt[1].max=90.; mt[2].min=-30.; mt[2].max=30;
-          }
-          if (mt[1].dd==0. && (mt[1].min != mt[1].max) && (mt[2].min != mt[2].max)){
-              // If Lune increment is not defined set default to 10 degree 
-              // If (mt[1].min == mt[1].max) && (mt[2].min == mt[2].max) and mt[1].dd==0 then do inversion at Fixed Lune point
-              mt[1].dd=10.; fprintf(stderr,"Warning: Increment not specified (-I flag); Setting it to 10\n");}
-      }
-      mt[2].dd=mt[1].dd;     // Same increment for ISO and CLVD
-      mt[1].par=mt[1].min;   
-      mt[2].par=mt[2].min;
-  }
-  else { // (search == 0) 
-    // use ranges for zhu parameters if "search = 0" 
-    scanf("%f%f", &(mt[1].par), &(mt[1].dd)); mt[1].min = -1.; mt[1].max = 1.;   // -1 to 1
-    scanf("%f%f", &(mt[2].par), &(mt[2].dd)); mt[2].min = -0.5; mt[2].max = 0.25;  //-0.5 to 0.25
-  }
-
-  //--------------------------------------------------
-  // grid.n[0] = Number of elements in strike
-  // grid.n[1] = Number of elements in dip
-  // grid.n[2] = Number of elements in rake
-  // grid.step[0] = step size for strike
-  // grid.step[1] = step size for dip
-  // grid.step[2] = step size for rake
-  //--------------------------------------------------
-  for(j=0;j<3;j++) {
-      // j==0 (strike); j==1 (Dip); j==2 (Rake); 
-      // Set search range  (-R flag) and increment (-I flag) for (stike, dip, rake) 
-      // See cap.pl for : search range =($str1, $str2, $dip1, $dip2, $rak1, $rak2);  increment = $deg;
-      scanf("%f%f%f",&x1,&x2,&grid.step[j]);       // x1 = starting point ($str1, $dip1, $rak1);  x2 = end point ($str2,$dip2,$rak2); grid.step[j] = increment size $deg)
-      grid.n[j] = rint((x2-x1)/grid.step[j]) + 1;  // number of search elements per parameter (for ending the loop); See error function
-      grid.x0[j] = x1;                             // Starting fot the search range per parameter; See error function
-      if (j==1 && x1==0){                          
-          // Don't ever use horizontal fault (dip=0)
-          // Change starting of dip search from 0 to the next increment 'grid.step[1]'
-          // This reduces the number of search elements in dip by 1
-          x1 = grid.step[j];
-          grid.x0[j] = grid.step[j];
-          grid.n[j] = rint((x2-x1)/grid.step[j]) + 1;
-      }
-  }
-  // Preallocate memory for orientation (strike,dip,rake) matrix
-  grid.err = (float *) malloc(grid.n[0]*grid.n[1]*grid.n[2]*sizeof(float));
-  if (grid.err == NULL ) {
-    fprintf(stderr,"fail to allocate memory for storing misfit errors\n");
-    return -1;
-  }
-
-    */
-
   // allocate memory for (strike, dip, rake)
   // grid.err = (float *) malloc(grid.n[0]*grid.n[1]*grid.n[2]*sizeof(float));
   grid.err = (float *) calloc(searchPar->nsol, sizeof(float));
@@ -384,8 +294,6 @@ int main (int argc, char **argv) {
     } else {
         fprintf(stderr,"done.\n\n");
     }
-
-    /* END prepare inversion variables */
 
   /** end -- get range of search parameters **/
 
@@ -826,30 +734,6 @@ int main (int argc, char **argv) {
     return -1;
   }
 
-  /************grid-search for full moment tensor***********/
-//  logf = fopen("log_diff","w");
-//  fclose(logf);
-
-  // maybe we label this section "cap messages"...
-  /*
-  if (search==0){
-    if (mt[1].dd>=1 || mt[2].dd>=1)
-      fprintf(stderr,"Warning: Possible error = Expecting grid-search\n(set search=1) or reduce the search increment (-J flag)\n");
-    fprintf(stderr,"----------starting line-search-----------\n");}
-  if (search==1){
-    if ((mt[1].dd<=1&&mt[1].dd!=0) || (mt[2].dd<=1&&mt[2].dd!=0))
-      fprintf(stderr,"Warning: Very fine grid-search = Expecting line-search\n(set search=0) or increase search increment (-J flag)\n");
-    fprintf(stderr,"----------starting grid-search-----------\n");
-
-    if(misfit_on_lune)
-    {
-        fprintf(stderr,"NOTE: misfit on lune output to file out.misfit.wf_\n");
-    }
-  }
-  if (search==2)
-     fprintf(stderr,"----------starting random-search-----------\n");
-     */
-
  INVERSION:
   fprintf(stderr,"\nBEGIN SEARCH\n\n"); 
 
@@ -927,12 +811,8 @@ int main (int argc, char **argv) {
   }
 
   fprintf(stderr,"Preparing out file ...\n");
-  //grid.n[0]=grid.n[1]=grid.n[2]=1;
-  //grid.x0[0]=sol.meca.stk; grid.x0[1]=sol.meca.dip; grid.x0[2]=sol.meca.rak;
-  //sol = error(nda,obs0,nfm,fm0,max_shft,m0,grid,fm_thr,tie);
   sprintf(mod_dep,"%s_%03d",model,depth);                       // rename .out file
   strcat(strcat(strcat(strcpy(tmp,eve),"/"),mod_dep),".out");   
-  //  strcat(strcat(strcat(strcpy(tmp,eve),"/"),dep),".out");   
   f_out=fopen(tmp,"w");
   fprintf(f_out,"Event %s Model %s FM %4d %9.6f %4d Mw %4.2f rms %9.3e %5d ERR %3d %3d %3d ISO %10.6f %3.2f CLVD %3.2f %3.2f VR %3.1f data2 %9.3e\n",eve,mod_dep,
 	  (int) rint(sol.meca.stk), sol.meca.dip, (int) rint(sol.meca.rak),
