a56520f5dccae7847c47a404ca27113c8c797c51
kslytherin <kksmith7@alaska.edu>
2020-01-24 15:43:10 -0900

    test function for carl

--- UTILS/matlab/run_add_picks2weight.m
+++ UTILS/matlab/run_add_picks2weight.m
@@ -3,8 +3,36 @@ close all
 clc
 
 % Nenana basin study
-weight_input = '/home/ksmith/REPOSITORIES/capuaf/20181113152641907/weight_body.dat'
-weight_output = '/home/ksmith/REPOSITORIES/capuaf/20181113152641907/weight_body_wpicks.dat'
+% evid = '20181113152641907';
+% evid = '20180916191154565';
+% evid = '20190925134513809';
+% evid = '20171230114316278';
+% evid = '20160711200557702';
+% evid = '20170613073936181';
+% evid = '20190117121355727';
+% evid = '20190624090423301';
+% evid = '20190624090423301';
+% evid = '20170527163305640';
+% evid = '20180825181551481';
+evid = 'Apr29MT/V1/20170429111548898';
+cap_path = '/home/ksmith/REPOSITORIES/capuaf/';
+case_no = 4;
+switch case_no 
+    case 1 
+    	weight_input = [cap_path evid '/weight.dat'];
+    	weight_output = [cap_path evid '/weight_wpicks_and_pol.dat'];
+    case 2
+    	weight_input = [cap_path evid '/weight_body.dat'];
+    	weight_output = [cap_path evid '/weight_body_wpicks_and_pol.dat']
+    case 3 
+    	weight_input = [cap_path evid '/weight_surf.dat'];
+    	weight_output = [cap_path evid '/weight_surf_wpicks_and_pol.dat']
+    case 4 
+    	weight_input = [cap_path evid '/weight_body_nobasin.dat'];
+	cap_path = '/home/carltape/REPOSITORIES/capuaf/';
+	weight_output = [cap_path evid '/weight_body_nobasin_wpicks_and_pol.dat']
+end
+
 idb = 1;            % Use AEC db
 HWIN_SEC = 2;       % this is really important because otime is different for AEC and IRIS
 iUseReviewed = 1;   % Analyst reviewed or unreviewed polarity
