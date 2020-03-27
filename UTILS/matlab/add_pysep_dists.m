065a9edce6493357b1b58046993b67ace87badfe
kslytherin <kksmith7@alaska.edu>
2020-03-26 17:50:14 -0800

    specify pysep distance grabs

---
+++ UTILS/matlab/add_pysep_dists.m
@@ -0,0 +1,44 @@
+clear all
+close all
+clc
+
+% obtaining MT distances retrospectively and writing to new obspy like file but specifies max stn dist grabs
+aggwtfile = '/home/ksmith/REPOSITORIES/capuaf/nenanabasin_MTs/weight_files/tail_file.txt';
+
+[stnm, pol, edist, PV_wt, PR_wt, SV_wt, SR_wt, ST_wt, P_arrival, P_len, S_arrival, S_len, waveform_shft] = read_cap_weight(aggwtfile);
+eidlength = 4+2+2+2+2+2+3; % corresponds to yyyy mm dd ...
+eiddaylength = 4+2+2;
+for s_i = 1:length(stnm)
+    old_eidset{s_i} = stnm{s_i}(1:eidlength);
+    old_eiddayset{s_i} = stnm{s_i}(1:eiddaylength);
+end
+edistmax = ceil(edist/25)*25;
+
+obspy_evs = '/home/ksmith/REPOSITORIES/manuscripts/kyle/papers/nenanabasin/data/nenanabasin';
+[otime,lon,lat,dep,mag,eid] = read_seis_obspy(obspy_evs);
+for e_i = 1:length(eid) 
+    eid_day{e_i} = eid{e_i}(1:eiddaylength); 
+end
+% start writing new file
+n = length(lon);
+
+filename = [obspy_evs '_obspy_pysep_dwnld.txt'];
+
+disp(['writing ' filename]);
+
+stfmt = '%3i  %s  %s %12.6f %12.6f %8.1f %6.2f %1.0f';
+
+fid = fopen(filename,'w');
+for ii = 1:n
+    nd_i = find(strcmp(eid_day{ii},old_eiddayset));
+    if ~isempty(nd_i)
+    	newedist = edistmax(nd_i);
+    else
+    	newedist = 1000;
+    end 
+    % note: depth is in meters
+    fprintf(fid,[stfmt '\n'],...
+        ii,eid{ii},datestr(otime(ii),'yyyy-mm-ddTHH:MM:SS.FFF'),...
+        lon(ii),lat(ii),dep(ii)*1000,mag(ii),newedist);
+end
+fclose(fid);
--- UTILS/matlab/use_old_weights.m
+++ UTILS/matlab/use_old_weights.m
@@ -1,4 +1,11 @@
-% this code might not work for stations with polarity informtion
+clear all
+close all
+clc
+
+% by Kyle Smith
+% created on 2020/03/26 
+% this code will copy weights and polarity from one weight file into another 
+
 oldfile = '/home/ksmith/REPOSITORIES/capuaf/MTs/Aug28MT/V1/20180828151844000/weight.dat';
 
 neweid = '20180828151843464';
@@ -13,11 +20,13 @@ end
 missing_stns = setdiff(stnm_o,stnm)
 added_stns = setdiff(stnm,stnm_o)
 
+% copy same information for "colocated stations"
 DEstns = {[neweid '.DE.UAF01..HH'],[neweid '.DE.UAF02..HH']};
 nearDEstns = {[neweid '.XV.FAPT..HH'],[neweid '.AK.NEA2..BH']};
 if ~isempty(intersect(added_stns,DEstns))
     [~,Ade,~] = intersect(stnm,DEstns,'stable');
     [~,Ande,~] = intersect(stnm,nearDEstns,'stable');
+    pol(Ade) = pol(Ade); 
     PV_wt(Ade) = PV_wt(Ande); 
     PR_wt(Ade) = PR_wt(Ande); 
     SV_wt(Ade) = SV_wt(Ande); 
@@ -30,6 +39,6 @@ end
 
 [C,IA,IB] = intersect(stnm,stnm_o,'stable');
 
+pol(IA) = pol_o(IB); 
 PV_wt(IA) = PV_wt_o(IB); PR_wt(IA) = PR_wt_o(IB); SV_wt(IA) = SV_wt_o(IB); SR_wt(IA) = SR_wt_o(IB); ST_wt(IA) = ST_wt_o(IB);
-
 write_cap_weight(newfile, stnm, pol, edist, PV_wt,PR_wt, SV_wt, SR_wt, ST_wt, P_arrival, P_len, S_arrival, S_len, waveform_shft);
